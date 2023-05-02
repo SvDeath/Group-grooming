904c917e0d89720789764465dbf30411279b24ce
aida.stenk <stenkovaaida@gmail.com>
2023-05-01 19:56:02 -0500

    GroupList2 was updated

--- src/Java_coding/GroupList2.java
+++ src/Java_coding/GroupList2.java
@@ -1,5 +1,20 @@
 package Java_coding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GroupList2 {
 
-}
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Roman Aliiev", "Aida Stenkova", "Mykita Gannota", "Kamal Khan Daadman", "Oyuchimeg Burentogtohk", "Maria Gabriela Rosa ledesma", "Deonis Chiperi", "Taha Ali", "Ganchimeg Sosorbaram"));
+
+        char ok = 9989;
+        char fail = 9899;
+
+
+        }
+    }
+
